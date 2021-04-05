0f6ab073680f22fcb615031f2f2a67311a11dfdc
kazuki-s <kazu827272@gmail.com>
2021-04-05 13:27:26 +0900

    Scanクラスを更新
    numberScanメソッド（キーボードからの入力を受付、ArrayListに格納）を追加。

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        Scan scan1 = new Scan();
+        scan1.numberScan();
+        //↓入力ができているかの確認。後で削除。1234
+        System.out.println(scan1.getNumberArr());
 
     }
 }
--- Scan.java
+++ Scan.java
@@ -1,3 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 public class Scan {
-    
+
+    //解答者の入力した値を格納する配列
+    private ArrayList<Integer> numberArr1 = new ArrayList<Integer>();
+
+    public ArrayList<Integer> getNumberArr() {
+        return this.numberArr1;
+    }
+
+    public void numberScan() {
+        System.out.println("0~9のうち3つの数字を入力してください。");
+        //後で削除
+        System.out.println(numberArr1.size());
+        for(;;) {
+            Scanner scanner = new Scanner(System.in);
+            int int1 = scanner.nextInt();
+            if(int1 < 1000 && int1 > 0) {
+                Integer[] integer1 = new Integer[3]; 
+                integer1[0] = Integer.valueOf(int1 / 100);
+                integer1[1] = Integer.valueOf(int1 % 100 / 10);
+                integer1[2] = Integer.valueOf(int1 % 10);
+                if(!(integer1[0].equals(integer1[1])) && !(integer1[0].equals(integer1[2])) && !(integer1[1].equals(integer1[2]))) {
+                    ArrayList<Integer> numberArr2 = new ArrayList<Integer>(Arrays.asList(integer1));
+                    for(int i = 0; i < 3; i++) {
+                        numberArr1.add(i,numberArr2.get(i));
+                    }
+                    break;
+                }
+            }
+            System.out.println("エラーです。もう一度入力してください。"); 
+        }
+    }    
+
+
+
 }
